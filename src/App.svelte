381165a13e9357a78b03c61fe371a093a758257a
= <=>
2020-02-27 21:49:50 -0300

    card

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,51 @@
 <script>
   let nome = "Luiz Carlos";
+  let idade = 49;
+  let titulo = "Professor";
+  let descricao = "Um ótimo professor. Dá boas notas.";
+  let imagem = "https://avatars0.githubusercontent.com/u/5073025?s=460&v=4";
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
+  .card {
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 1);
+    max-width: 30rem;
+    border-radius: 5px;
+    margin: 2rem 0;
   }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 7rem;
   }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  .foto {
+    width: 100px;
   }
 </style>
 
-<main>
-  <h1>Olá {nome}!</h1>
-  <p>Bem vindo ao maravilhoso mundo do Svelte!</p>
-</main>
+<h1>Alô {nome}, minha idade é {idade}.</h1>
+<input type="text" bind:value={nome} />
+<br />
+<input type="text" bind:value={idade} />
+<br />
+<input type="text" bind:value={imagem} size="100" />
+<br />
+<input type="text" bind:value={titulo} />
+<br />
+<textarea bind:value={descricao} />
+
+<div class="card">
+  <header>
+    <div>
+      <img class="foto" src={imagem} alt={nome} />
+    </div>
+    <div>
+      <h1>{nome}</h1>
+      <h2>{titulo}</h2>
+    </div>
+  </header>
+  <div>
+    <p>{descricao}</p>
+  </div>
+</div>
